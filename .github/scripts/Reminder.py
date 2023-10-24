72e84609c60d78f451af04146138e6cfe0419df1
K7413dS433d <khaledsaied2002@gmail.com>
2023-10-24 20:56:50 +0300

    try another api from rabid api Quotes

--- .github/scripts/Reminder.py
+++ .github/scripts/Reminder.py
@@ -8,7 +8,10 @@ import telebot
 Reminder_Bot_Token = os.environ.get('K7413D_REMINDER_BOT_TOKEN')
 
 # They said so api
-They_said_so_Token = os.environ.get('THEY_SAID_SO_TOKEN')
+# They_said_so_Token = os.environ.get('THEY_SAID_SO_TOKEN')
+
+# Rabid api request 'Quotes'
+Rapid_Api_Quotes_token = os.environ.get('RAPID_API_QUOTES_TOKEN')
 
 
 # git user id from environment variables
@@ -19,25 +22,44 @@ current_date = datetime.date.today()  # current date
 
 current_day_name = current_date.strftime("%A")  # Full day name
 
-
-# They said so request
 requestRes = ""  # Request result
+# They said so request ##########################################################################
 
+# headers = {
+#     'X-TheySaidSo-Api-Secret': f'{They_said_so_Token}'
+# }
+
+# response = requests.get("https://quotes.rest/qod", headers=headers)
+
+
+# if response.status_code == 200:
+#     data = response.json()
+#     quote = data['contents']['quotes'][0]['quote']
+#     author = data['contents']['quotes'][0]['author']
+#     requestRes = f"{quote} \n Author: \n {author}"
+# else:
+#     requestRes = f"Api error with status code: {response.status_code} "
+
+################################################################################################
+
+# Rabid api request 'Quotes'####################################################################
+url = "https://quotes15.p.rapidapi.com/quotes/random/"
 
 headers = {
-    'X-TheySaidSo-Api-Secret': f'{They_said_so_Token}'
+    "X-RapidAPI-Key": f'{Rapid_Api_Quotes_token}',
+    "X-RapidAPI-Host": "quotes15.p.rapidapi.com"
 }
 
-response = requests.get("https://quotes.rest/qod", headers=headers)
-
+response = requests.get(url, headers=headers)
 
 if response.status_code == 200:
     data = response.json()
-    quote = data['contents']['quotes'][0]['quote']
-    author = data['contents']['quotes'][0]['author']
+    quote = data['contents']
+    author = data['originator']['name']
     requestRes = f"{quote} \n Author: \n {author}"
 else:
     requestRes = f"Api error with status code: {response.status_code} "
+################################################################################################
 
 
 # handel the message
